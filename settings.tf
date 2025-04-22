32ab55fe63efc2745ed99641c13a8fe7bdbe35aa
anton.aleksandrov <anton.aleksandrov@jetbrains.com>
2025-04-22 16:13:50 +0200

    added setting.tf with default config + .env file with an empty value

---
+++ settings.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    teamcity = {
+      source = "jetbrains/teamcity"
+    }
+  }
+}
+
+provider "teamcity" {
+  host = "https://teamcity-terraform-demo.teamcity.com/"
+  token = "env.TEAMCITY_TOKEN"
+}
